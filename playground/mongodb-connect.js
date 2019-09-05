5d05197e9775b067b94b44c9e01a9aee5cb7497d
github_sagarsingla14 <sagarrock1499@gmail.com>
2019-09-05 16:11:16 +0530

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,91 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+var {Age,name} = user;
+console.log(name ,  Age);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to mongoDB server');
+  }
+
+  const db = client.db('TodoApp');
+  db.collection('Users').insertOne({
+    Name : "Sagar Singla",
+    Age : 19,
+    Skills : "Sport Programming"
+  },(err , result) =>{
+    if(err){
+      return console.log('Unable to insert into database');
+    }
+    console.log(JSON.stringify(result.ops,undefined,3));
+    console.log(result.ops[0]._id.getTimestamp());
+  });
+
+  client.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+// var obj = new ObjectID();
+// console.log(obj);
+// var user = {
+  //   name : "Sagar Singla",
+  //   Age : 20
+  // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) => {
+  //   if(err){
+    //     return console.log('Unable to connect Database server !!');
+    //   }
+    //   console.log('Connected To mongoDB Server Successfully');
+    //
+    //   const db = client.db('TodoApp');
+    //   db.collection('Todos').insertOne({
+      //     text : "Hello",
+      //     Connected : false
+      //   }, (err,result) =>{
+        //     if(err){
+          //       return console.log('Unable to insert todo !');
+          //     }
+          //
+          //     console.log(JSON.stringify(result.ops, undefined,2));
+          //   });
+          //
+          //   client.close();
+          // });
