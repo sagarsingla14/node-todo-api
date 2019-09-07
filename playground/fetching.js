d08d12fe7cdbbe0ccc0b772c006e068f268789b6
github_sagarsingla14 <sagarrock1499@gmail.com>
2019-09-07 23:58:31 +0530

    fetching Data from Database added Counting docs

--- playground/fetching.js
+++ playground/fetching.js
@@ -6,6 +6,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   }
   console.log('Connected Successfully to Mongo Server');
   const db = client.db('TodoApp');
+
+
+
+  // Count Function Counts the Docs in the Current Database
+  console.log('Counting Docs ');
+  db.collection('Users').find().count().then((count) => {
+    console.log(count);
+  } , (err) => {
+    console.log('Unable to Fetch the data');
+  });
+  client.close();
   // toArray parse data into Object of Arrays
 
   db.collection('Users').find({
@@ -25,4 +36,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Unable to Fetch the data');
   });
   client.close();
+
+
+
 });
