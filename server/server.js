731a8001d360b3f5c17225a1d61fb23a8038b614
github_sagarsingla14 <sagarrock1499@gmail.com>
2019-09-10 16:43:39 +0530

    Added Validators and Defaluts

--- server/server.js
+++ server/server.js
@@ -7,24 +7,47 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo' , {
   text : {
-    type : String
+    type : String ,
+    required : true
   } ,
   completed : {
-    type : Boolean
+    type : Boolean,
+    Default : false
   } ,
   completedAt : {
-    type : Number
+    type : Number ,
+    Default : null
   }
 });
 
 var newTodo = new Todo({
   text : 'Cook Dinner',
   completed : false,
-  type : 1
+  completedAt : 1
 });
 
 newTodo.save().then((docs) => {
   console.log(JSON.stringify(docs));
 }, (err) => {
   console.log('Unable to save ! ' , err);
+});
+
+// Added Validators and Defaults
+var User = mongoose.model('User', {
+  email : {
+    type : String,
+    required : true,
+    trim : true,
+    minlemgth : 1
+  }
+});
+
+var user = new User({
+  email : 'sagar@example.com'
+});
+
+user.save().then((docs) => {
+  console.log(JSON.stringify(docs , undefined , 2));
+},(err) => {
+  console.log('Unable to Save', e);
 });
