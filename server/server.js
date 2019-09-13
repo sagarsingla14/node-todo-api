03a28311dcdcde08377511d95b21d743cc58ab50
github_sagarsingla14 <sagarrock1499@gmail.com>
2019-09-14 00:50:45 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,6 +30,26 @@ app.get('/todos' , (req , res) => {
     res.status(400).send(e);
   });
 });
+
+// Getting An individual response
+
+app.get('/todos/:id' , (req , res) => {
+  var id = req.params.id;
+  if(! ObjectId.isValid(id)){
+    res.status(404).send();
+    return console.log('Id not Valid');
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      res.status(404).send();
+      return console.log('Id not found');
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(404).send();
+  });
+});
+
 
 app.listen(3000 , () => {
   console.log('Started on port 3000');
